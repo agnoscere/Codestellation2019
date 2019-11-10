7996cdf7cf54e7688c8c06811a584e55a49e24ec
danielmattson <daniel18mattson@gmail.com>
2019-11-10 00:47:04 -0500

    basic translation support
    added language translation to hard coded russian. for some reason it always dds an exclamation point.
    the server also defaults to port 50005

--- chat-app/server.js
+++ chat-app/server.js
@@ -24,22 +24,22 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-	
+
 	usersOn = socket.conn.server.clientsCount;
 	io.emit('usersOn', usersOn);
-		
+
 	socket.on('userData', function(userData){
 		socket.username = userData.username;
 	})
-	
+
 	socket.on('chat message', function(msg){
 		io.emit('chat message', msg);
 	})
-	
+
 	socket.on('notification', function(notification){
 		io.emit('notification', notification);
 	});
-	
+
 	socket.on('disconnect', function(){
 		usersOn -= 1;
 		io.emit('usersOn', usersOn);
@@ -54,9 +54,74 @@ var ipaddress = process.env.PORT || "127.0.0.1";
 
 //Same as above, but with port
 //If you want to load on localhost onto a different port, change 4000 to whatever port you please
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 50005;
 
 http.listen(port, ipaddress, function(){
-	console.log('Running on 127.0.0.1:8080')
+	console.log('Running on 127.0.0.1:50005')
 })
 
+var express = require('express')
+var app = express()
+var path = require('path')
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
+/////////////////////////////////////////////////////////////////
+//This fixed the issue with long disconnecting times in browsers
+//The interval checks if player is connected every 1 seconds
+//If the player is disconnected for 5 second, they get booted
+/////////////////////////////////////////////////////////////////
+io.set('heartbeat interval', 1000);
+io.set('heartbeat timeout', 5000);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//     console.log(googleRes.text);
+//     console.log(googleRes.from.language.iso);
+// 		serverRes.send(googleRes.text);
+app.get('/chat', function(req, res){
+	res.sendFile(__dirname + '/templates/chat.html');
+});
+
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/templates/home.html');
+});
+
+io.on('connection', function(socket){
+
+	usersOn = socket.conn.server.clientsCount;
+	io.emit('usersOn', usersOn);
+
+	socket.on('userData', function(userData){
+		socket.username = userData.username;
+	})
+
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	})
+
+	socket.on('notification', function(notification){
+		io.emit('notification', notification);
+	});
+
+	socket.on('disconnect', function(){
+		usersOn -= 1;
+		io.emit('usersOn', usersOn);
+		io.emit('notification', socket.username + " has disconnected.");
+	});
+});
+
+//Server configuration stuff
+
+//Tries to find variable for openshift ip, if nothing then loads to localhost
+var ipaddress = process.env.PORT || "127.0.0.1";
+
+//Same as above, but with port
+//If you want to load on localhost onto a different port, change 4000 to whatever port you please
+var port = process.env.PORT || 50005;
+
+http.listen(port, ipaddress, function(){
+	console.log('Running on 127.0.0.1:50005')
+})
+>>>>>>> before discard
